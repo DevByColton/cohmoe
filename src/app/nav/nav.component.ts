a77be342a361a8a62c336e9ac0a05d7112db60f0
Colton Morales <developmentbycolton@outlook.com>
2024-05-14 18:59:53 -0500

    Working on nav updates

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,12 +1,13 @@
 import {Component, inject} from '@angular/core';
 import {ButtonComponent} from "../shared/button/button.component";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
-    ButtonComponent
+    ButtonComponent,
+    RouterLink
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
