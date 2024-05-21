ee83da44066ecd2c54796791dcb9eb9821635c95
Colton Morales <developmentbycolton@outlook.com>
2024-05-21 14:40:23 -0500

    Added a blog route Added button in nav for blog Clean up styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent) }
+  {
+    path: '',
+    loadComponent: () => import('./home/home.component').then(mod => mod.HomeComponent),
+    title: 'Cohmoe - Home'
+  },
+  {
+    path: 'blog',
+    loadComponent:() => import('./blog/blog.component').then(mod => mod.BlogComponent),
+    title: 'Cohmoe - Blog'
+  },
+  {
+    path: '**',
+    loadComponent:() => import('./page-not-found/page-not-found.component').then(mod => mod.PageNotFoundComponent),
+    title: 'Cohmoe - Page Not Found'
+  }
 ];
