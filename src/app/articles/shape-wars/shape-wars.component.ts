3bf9d94ee0066233786f712ec531d3dd5f536371
Colton <developmentbycolton@outlook.com>
2025-10-07 07:06:42 -0500

    Embedded YouTube video link to shape wars gameplay

--- src/app/articles/shape-wars/shape-wars.component.ts
+++ src/app/articles/shape-wars/shape-wars.component.ts
@@ -1,6 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {ArticleCardComponent} from '../../shared/components/article-card/article-card.component';
 import {ButtonComponent} from '../../shared/button/button.component';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-shape-wars',
@@ -13,5 +14,8 @@ import {ButtonComponent} from '../../shared/button/button.component';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ShapeWarsComponent {
-
+  
+  private domSanitizer = inject(DomSanitizer);
+  
+  safeUrl: SafeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/1bVX6a6VZ1I?si=E9bylaEOMz1MolYv');
 }
