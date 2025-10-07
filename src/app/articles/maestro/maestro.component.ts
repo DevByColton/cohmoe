7fd7b4b5e67d0cbcbed45f2551c441e14e79bbb5
Colton <developmentbycolton@outlook.com>
2025-10-07 08:53:17 -0500

    Removed unneeded assets and made star keeper and maestro videos embedded from YouTube

--- src/app/articles/maestro/maestro.component.ts
+++ src/app/articles/maestro/maestro.component.ts
@@ -3,6 +3,7 @@ import {ArticleCardComponent} from '../../shared/components/article-card/article
 import {ButtonComponent} from '../../shared/button/button.component';
 import {Router, RouterLink} from '@angular/router';
 import {AppRoutes} from '../../app.routes';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-maestro',
@@ -18,7 +19,10 @@ import {AppRoutes} from '../../app.routes';
 export class MaestroComponent {
 
   private router: Router = inject(Router);
+  private domSanitizer = inject(DomSanitizer);
   readonly appRoutes = AppRoutes;
+
+  safeUrl: SafeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/yQ1kUxZKjJg?si=3S74NkOySzq5xhvG');
   
   playMaestroClick(): void {
     this.router.navigate([AppRoutes.PLAY_MAESTRO]).then(() => {});
--- src/app/articles/star-keeper/star-keeper.component.ts
+++ src/app/articles/star-keeper/star-keeper.component.ts
@@ -1,6 +1,7 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
 import {ArticleCardComponent} from '../../shared/components/article-card/article-card.component';
 import {ButtonComponent} from '../../shared/button/button.component';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-star-keeper',
@@ -14,4 +15,7 @@ import {ButtonComponent} from '../../shared/button/button.component';
 })
 export class StarKeeperComponent {
 
+  private domSanitizer = inject(DomSanitizer);
+
+  safeUrl: SafeUrl = this.domSanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/1TJO7yTwkwY?si=YWE--hRUvGex0eOm');
 }
